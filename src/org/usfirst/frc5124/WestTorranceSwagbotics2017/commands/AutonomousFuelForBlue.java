6464cda23be0d7b5ddde228c07db2f3b533815a9
Zach Jicha <zachjicha13@gmail.com>
2017-03-09 09:10:04 -0800

    auto

--- src/org/usfirst/frc5124/WestTorranceSwagbotics2017/commands/AutonomousFuelForBlue.java
+++ src/org/usfirst/frc5124/WestTorranceSwagbotics2017/commands/AutonomousFuelForBlue.java
@@ -9,5 +9,15 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class AutonomousFuelForBlue extends CommandGroup {
 
     public AutonomousFuelForBlue() {
+    	//move up ~2/3 or so the distance it takes to reach the base line starting from right next to the boiler
+    	addSequential(new AutoDriveByPID(60));
+    	//Turn towards boiler
+    	addSequential(new AutoTurnWithGyro(20));
+    	//move towards boiler
+    	addSequential(new AutoDriveByPID(63));
+    	//addSequential(new AutoTrashDrive(4, -1));
+    	//turn on conveyor and shooters for rest of auto
+    	addParallel(new FuelInjectorConveyorToShooters());
+    	addSequential(new ShooterStartShooting());
     }
 }
--- src/org/usfirst/frc5124/WestTorranceSwagbotics2017/commands/AutonomousGearBasicStraight.java
+++ src/org/usfirst/frc5124/WestTorranceSwagbotics2017/commands/AutonomousGearBasicStraight.java
@@ -8,5 +8,9 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class AutonomousGearBasicStraight extends CommandGroup {
 
     public AutonomousGearBasicStraight() {
+    	addSequential(new AutoDriveByPID(75));
+    	//go to lift
+    	addSequential(new AutoKamikazeGear());
+    	addSequential(new GearHolderSafelyOpen());
     }
 }
