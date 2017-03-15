32a1394ef5e09bec55aa92c0b4273ec2a33fbb8e
Zach <zachjicha13@gmail.com>
2017-03-14 23:21:47 -0700

    pathfinder

---
+++ src/org/usfirst/frc5124/WestTorranceSwagbotics2017/commands/Auto/AutonomousUsePathfinder.java
@@ -0,0 +1,31 @@
+package org.usfirst.frc5124.WestTorranceSwagbotics2017.commands.Auto;
+
+import org.usfirst.frc5124.WestTorranceSwagbotics2017.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class AutonomousUsePathfinder extends Command {
+
+    public AutonomousUsePathfinder() {
+        requires(Robot.drivetrain);
+    }
+
+    protected void initialize() {
+    }
+
+    protected void execute() {
+    }
+
+    protected boolean isFinished() {
+        return false;
+    }
+
+    protected void end() {
+    }
+
+    protected void interrupted() {
+    }
+}
